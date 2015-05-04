d3084a20b5da427b08bef220e6ec1100d17c1dff
Ólafur Páll Geirsson <olafurpg@gmail.com>
2015-05-04 22:52:15 +0200

    Add game steps and isOver

--- leon-go/src/main/scala/sav/go/leon/Go.scala
+++ leon-go/src/main/scala/sav/go/leon/Go.scala
@@ -7,8 +7,19 @@ case object WhiteCell extends Cell
 case object BlackCell extends Cell
 case object EmptyCell extends Cell
 
-case class Board(b: Seq[Seq[Cell]]) {
-  def this(n: Int) = this(List.fill(n)(List.fill(n)(EmptyCell)))
+case class Board(n: Int, cells: Map[(Int, Int), Cell]) {
+  def this(n: Int) = this(n, Map.empty)
+
+  def inRange(x: Int) = 0 <= x && x < n
+
+  def put(c: Cell, x: Int, y: Int) = {
+    require(inRange(x) && inRange(y) && !cells.contains((x, y)))
+    Board(n, cells + ((x -> y) -> c))
+  }
+
+  def full: Boolean = cells.size == n * n
+
+  def playerCells(p: PlayerType): Set[(Int, Int)] = cells.withFilter(_._2 == p.cell).map(_._1).toSet
 }
 
 sealed trait PlayerType {
@@ -16,38 +27,69 @@ sealed trait PlayerType {
     case WhitePlayer => WhiteCell
     case BlackPlayer => BlackCell
   }
+  def nextPlayer: PlayerType = this match {
+    case WhitePlayer => BlackPlayer
+    case BlackPlayer => WhitePlayer
+  }
+  def previousPlayer: PlayerType = nextPlayer
 }
 
 case object WhitePlayer extends PlayerType
 case object BlackPlayer extends PlayerType
 
 
-trait Step
+sealed trait Step
 case object Pass extends Step
-case object Surrender extends Step
-case class Move(x: Int, y: Int) extends Step
+case class Place(x: Int, y: Int) extends Step
 
 case class State(b: Board, activePlayer: PlayerType) {
-  def next(m: Step): State = activePlayer match {
-    case WhitePlayer => State(b, BlackPlayer)
-    case BlackPlayer => State(b, WhitePlayer)
+  def next(m: Step): State = m match {
+    case Pass => State(b, activePlayer.nextPlayer)
+    case Place(x, y) => State(b.put(activePlayer.cell, x, y), activePlayer.nextPlayer)
   }
-  def isOver: Boolean = false
+
+  def lastStep(prevState: State): Step = {
+    val k = b.playerCells(activePlayer.previousPlayer) -- prevState.b.playerCells(activePlayer)
+    // Leon must verify that this is never more than one
+    k.headOption.map { case (x, y) =>
+        Place(x, y)
+    }.getOrElse(Pass)
+  }
+
+  def isOver: Boolean = b.full
 }
 
 object State {
   def apply(n: Int): State = State(new Board(n), BlackPlayer)
 }
 
-case class Game(s: List[State]) {
+case class Game(states: List[State]) {
 //  require(s.length > 0)
 
-  val activePlayer: PlayerType = s.head.activePlayer
+  def state: State = {
+    require(states.length > 0)
+    states.head
+  }
 
-  def isOver: Boolean = s.head.isOver
+  def lastGame: Game = {
+    require(states.length > 0)
+    Game(states.tail)
+  }
+
+  def lastStep: Step = {
+    require(states.length >= 2)
+    state.lastStep(lastGame.state)
+  }
+
+  val activePlayer: PlayerType = state.activePlayer
+
+  def isOver: Boolean = lastStep == Pass && lastGame.lastStep == Pass
+
+  // TODO:
+  def steps: List[Step] = ???
 
   def move(m: Step): Game = {
-    Game(s.head.next(m) :: s)
+    Game(states.head.next(m) :: states)
   }
 }
 
@@ -79,9 +121,9 @@ trait Player {
 }
 
 case object HumanPlayer extends Player {
-  override def move(g: Game): Step = Move(0, 0)
+  override def move(g: Game): Step = Place(0, 0)
 }
 
 case object ComputerPlayer extends Player {
-  override def move(g: Game): Step = Move(0, 0)
+  override def move(g: Game): Step = Place(0, 0)
 }
