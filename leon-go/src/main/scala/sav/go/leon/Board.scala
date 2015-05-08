3a3c954313635f550f40d5b8f27db2a05c44a547
Ólafur Páll Geirsson <olafurpg@gmail.com>
2015-05-08 16:42:25 +0200

    Add InclusiveRange

--- leon-go/src/main/scala/sav/go/leon/Board.scala
+++ leon-go/src/main/scala/sav/go/leon/Board.scala
@@ -24,7 +24,7 @@ case class Board(n: Int, cells: GoMap) {
 
   def at(p: Point): Cell = at(p.x, p.y)
 
-  val r = 1 to n
+  val r = InclusiveRange.toList(1, n)
 
   def board = r.map(x => r.map(y => at(x, y)))
 
