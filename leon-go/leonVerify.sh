ca56ca1d21078b1ce4dcab3e0494959d5bd1db1e
Ólafur Páll Geirsson <olafurpg@gmail.com>
2015-06-06 16:21:02 +0200

    Remove scalajs

--- leon-go/leonVerify.sh
+++ leon-go/leonVerify.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 echo Executing Leon...
 
-leon --debug=timers --timeout=10 \
+     # --functions="connectedComponent" \
+leon --debug=timers --timeout=3 \
      --solvers=smt-z3 \
-     --functions="connectedComponent" \
      src/main/scala/go/collection/*.scala \
      src/main/scala/go/core/*.scala \
      src/main/scala/go/util/{conversions,Range,Logic}.scala
